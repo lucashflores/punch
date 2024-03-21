bc0c72c5ac22c252f065ca853ebbc7048baf84e4
lucashflores <lucas.henrique.flores2001@gmail.com>
2024-03-21 11:10:32 -0300

    Initial Commit

---
+++ Punch/PunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchApp.swift
+//  Punch
+//
+//  Created by Lucas Flores on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

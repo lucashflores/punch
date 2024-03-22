fac646b61107f8d7e2a19a3db55626c59a93fa32
lucashflores <lucas.henrique.flores2001@gmail.com>
2024-03-21 23:26:08 -0300

    first prototype

--- Punch/GameScene.swift
+++ Punch/GameScene.swift
@@ -9,76 +9,67 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var hitSack: SKNode!
+    private var hitsLabel: SKLabelNode!
+    private var progressBarParent: SKNode!
+    private var progressBar: SKNode!
+    private var strengthLabel: SKLabelNode!
+    private var hits = 0
+    private var strength = 1
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        hitSack = self.childNode(withName: "hitsack")
+        hitsLabel = self.childNode(withName: "hits_label") as? SKLabelNode
+        hitsLabel.fontName = "AvenirNext-Bold"
+        strengthLabel = self.childNode(withName: "strength_label") as? SKLabelNode
+        strengthLabel.fontName = "AvenirNext-Bold"
+        progressBarParent = self.childNode(withName: "progress_bar")
+        progressBar = self.progressBarParent.childNode(withName: "progress")
     }
     
     
+    func incrementProgressBar(amount: CGFloat) {
+        if (progressBar.xScale >= 0.99) {
+            progressBar.xScale = 0
+            strength += 1
+            strengthLabel.text = "STR \(strength)"
+        }
+        else if (progressBar.xScale + amount > 0.99) {
+            progressBar.xScale = 0.99
+            progressBar.position.x = progressBar.xScale * 50 + 0.5
+        }
+        else {
+            progressBar.xScale += amount
+            progressBar.position.x = progressBar.xScale * 50 + 0.5
+        }
+        
+    }
+    
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+        if hitSack.contains(pos) {
+            hits += 1
+            hitsLabel.text = "\(hits) hits!"
+            incrementProgressBar(amount: 0.04)
         }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
